02b64abc3fe906bca72ea122221afd71bd532f7b
Harshit <itsharshitraj@gmail.com>
2025-01-08 10:46:15 +0530

    Second program added

---
+++ AddTwoNumbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class AddTwoNumbers{
+ public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Give two numbers as input to add");
+
+   int a=sc.nextInt();
+   int b=sc.nextInt();
+
+   System.out.println("sum = " + (a+b));
+}
+
+}
